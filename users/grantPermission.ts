465755ce03dd0244a1546a0c76a736548e496f26
Maxime Moreillon <m.moreillon@gmail.com>
2024-10-08 09:38:39 +0900

    security comment

--- users/grantPermission.ts
+++ users/grantPermission.ts
@@ -7,6 +7,9 @@ async function main() {
   const db = client.db("admin")
 
   const username = "test"
+  // Does this really prevent users from accessing others' DBs?
+  // For instance, user john-doe would create john-doe-db
+  // user john could create john-doe-db too
   const dbName = `${username}-mydb`
   const roles = [
     {
