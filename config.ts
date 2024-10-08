cd12ff8e4a401ed6097722bacafd6c03b56d80fc
Maxime Moreillon <m.moreillon@gmail.com>
2024-10-08 12:05:47 +0900

    removed unused component

--- config.ts
+++ config.ts
@@ -1,10 +1,6 @@
 export const {
   DB_HOST = "localhost",
   DB_PORT = "5432",
-  DB_USER = "postgres",
-  DB_PASSWORD = "password",
-  DB_USE_SSL,
-  DB_INSECURE,
   JWT_SECRET = "sh...",
   TOKEN_COOKIE_NAME = "self_service_nosql_token",
   SESSION_COOKIE_NAME = "self_service_nosql_session",
